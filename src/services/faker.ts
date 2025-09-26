9945cb7eb8b870f7cde798ed23b0deeed423580c
DanbookAir <danbookair@DanbookAirs-MacBook-Air.local>
2025-09-25 18:12:48 -0600

    implement quantity arg

--- src/services/faker.ts
+++ src/services/faker.ts
@@ -1,6 +1,6 @@
-export async function fakerFetch(resource: string) {
+export async function fakerFetch(resource: string, quantity?: number) {
     const FAKER_BASE_URL = "https://fakerapi.it/api/v2"
-    const response = await fetch(`${FAKER_BASE_URL}/${resource}`)
+    const response = await fetch(`${FAKER_BASE_URL}/${resource}?_quantity=${quantity}`)
 
     return response.json()
 }
