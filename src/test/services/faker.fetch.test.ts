5e54f280d0392ff0a1172d998a9a74688cd7ca50
DanbookAir <danbookair@DanbookAirs-MacBook-Air.local>
2025-09-25 18:32:50 -0600

    Add test fixture and mock api call

--- src/test/services/faker.fetch.test.ts
+++ src/test/services/faker.fetch.test.ts
@@ -1,6 +1,46 @@
 import { describe, it, expect, beforeEach, vi } from "vitest"
 
-const FIXTURE = { status: "OK", code: 200, total: 1, data: [{ title: "Example" }] }
+const FIXTURE = { status: "OK", code: 200, total: 3, data: [{
+      title: "mip moop",
+      author: "woopy",
+      genre: "doop",
+      description: "bloop mip",
+      isbn: "9780000000001",
+      image: "https://picsum.photos/seed/birthday",
+      published: "09-20-2025",
+      publisher: "bop boop"
+    },
+    {title: "moop mip",
+      author: "dooply",
+      genre: "doop",
+      description: "bloop mip",
+      isbn: "9780000000001",
+      image: "https://picsum.photos/seed/birthday",
+      published: "09-20-2025",
+      publisher: "bap boop"
+    },
+    { title: "woop moop",
+      author: "moopy",
+      genre: "doop",
+      description: "bloop oop",
+      isbn: "9780000000001",
+      image: "https://picsum.photos/seed/birthday",
+      published: "09-20-2025",
+      publisher: "boop boop"
+    }]
+  }
+
+let fetchSpy: ReturnType<typeof vi.spyOn>;
+
+beforeEach(() => {
+  process.env.FAKER_BASE_URL = "https://fakerapi.it/api/v2";
+
+  fetchSpy = vi.spyOn(global, "fetch" as any).mockResolvedValue({
+    ok: true,
+    json: async () => FIXTURE,
+    status: 200,
+  } as any);
+})
 
 describe("fakerService.fetchBooks", () => {
   it("can be called called for the books resource", async () => {
@@ -8,6 +48,10 @@ describe("fakerService.fetchBooks", () => {
     const json = await fetchBooks()
     expect(json.code).toEqual(200)
     expect(json.status).toEqual('OK')
+    expect(json.data[0]).toMatchObject({
+      title:  "mip moop",
+      author:  "woopy",
+    })
   })
 
   it("can return a specific quantity of the books resource", async () => {
