2f65009c9ee1174cd99611d0826e9e4721e16e6f
DanbookAir <danbookair@DanbookAirs-MacBook-Air.local>
2025-09-25 18:16:03 -0600

    refine generic faker fetch call to faker books resource call

--- src/services/faker.ts
+++ src/services/faker.ts
@@ -1,6 +1,6 @@
-export async function fakerFetch(resource: string, quantity?: number) {
+export async function fetchBooks(quantity?: number) {
     const FAKER_BASE_URL = "https://fakerapi.it/api/v2"
-    const response = await fetch(`${FAKER_BASE_URL}/${resource}?_quantity=${quantity}`)
+    const response = await fetch(`${FAKER_BASE_URL}/books?_quantity=${quantity}`)
 
     return response.json()
 }
--- src/test/services/faker.fetch.test.ts
+++ src/test/services/faker.fetch.test.ts
@@ -2,17 +2,17 @@ import { describe, it, expect, beforeEach, vi } from "vitest"
 
 const FIXTURE = { status: "OK", code: 200, total: 1, data: [{ title: "Example" }] }
 
-describe("fakerService.fakerFetch", () => {
+describe("fakerService.fetchBooks", () => {
   it("can be called called for the books resource", async () => {
-    const { fakerFetch } = await import("@/services/faker")
-    const json = await fakerFetch('books')
+    const { fetchBooks } = await import("@/services/faker")
+    const json = await fetchBooks()
     expect(json.code).toEqual(200)
     expect(json.status).toEqual('OK')
   })
 
   it("can return a specific quantity of the books resource", async () => {
-    const { fakerFetch } = await import("@/services/faker")
-    const json = await fakerFetch('books', 3)
+    const { fetchBooks } = await import("@/services/faker")
+    const json = await fetchBooks(3)
     expect(json.code).toEqual(200)
     expect(json.status).toEqual('OK')
     expect(json.total).toEqual(3)
