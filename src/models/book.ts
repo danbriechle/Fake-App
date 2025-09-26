fa2b49c132dbceda4453772c826bb678cfd31085
DanbookAir <danbookair@DanbookAirs-MacBook-Air.local>
2025-09-26 11:51:43 -0600

    Add book model

---
+++ src/models/book.ts
@@ -0,0 +1,30 @@
+import type { BookApi } from "@/types/book-api";
+
+export class BookModel {
+  title: string;
+  author: string;
+  genre: string;
+  description: string;
+  isbn: string;
+  image: string;
+  published: string;
+  publisher: string;
+
+  private readonly seed: string;
+
+  constructor(raw: BookApi) {
+    this.seed = raw.isbn;
+    this.title = raw.title;
+    this.author = raw.author;
+    this.genre = raw.genre;
+    this.description = raw.description;
+    this.isbn = raw.isbn;
+    this.image = `https://picsum.photos/seed/${encodeURIComponent(seed)}/480/320`
+    this.published = raw.published;
+    this.publisher = raw.publisher;
+  }
+
+  static bookList(list: BookApi[]) {
+    return list.map(b => new BookModel(b));
+  }
+}
